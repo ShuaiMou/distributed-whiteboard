9b09aaadb6198f5d64900bce08a244e948553972
chenyuguo <49013160+chenyuguo@users.noreply.github.com>
2019-10-13 21:25:52 +1100

    aaa

--- createWhiteBoard/src/main/java/remoteInterface/Client.java
+++ createWhiteBoard/src/main/java/remoteInterface/Client.java
@@ -6,6 +6,6 @@ import java.rmi.RemoteException;
 public interface Client extends Remote{
     void showMessage(String message) throws RemoteException;
     String getUsername() throws RemoteException;
-    boolean hintWindow(String message) throws RemoteException;
+    boolean managerLogin(String message) throws RemoteException;
 
 }
--- createWhiteBoard/src/main/java/remoteInterface/Communication.java
+++ createWhiteBoard/src/main/java/remoteInterface/Communication.java
@@ -4,9 +4,9 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Communication extends Remote{
-            void sendMessage(Client client, String message) throws RemoteException;
+            void sendMessage(Client client,String message) throws RemoteException;
             void showHint(String message) throws RemoteException;
-            void checkLogin(Client client) throws RemoteException;
-
+            void managerLogin(Client client) throws RemoteException;
+            void collaboratorLogin(Client client) throws RemoteException;
 
 }
