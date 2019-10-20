544e864168b8596585aaf4a0c8b750b4829e3b42
ShuaiMou <shuaimou77@gmail.com>
2019-10-20 15:57:25 +1100

    saul

--- whiteboardServer/src/main/java/server/ServerImpl.java
+++ whiteboardServer/src/main/java/server/ServerImpl.java
@@ -118,9 +118,9 @@ public class ServerImpl extends UnicastRemoteObject implements Communication {
 
     public void addCommands(List<Integer> pointss, Color color, String command, Client client, boolean flag, String input) throws RemoteException {
         for (Client c : users){
-            if (!client.getUsername().equals(c.getUsername()) ){
+//            if (!client.getUsername().equals(c.getUsername()) ){
                 cachedCommands.get(c).add(new Object[]{pointss,color,command,flag,input});
-            }
+//            }
         }
 
 
